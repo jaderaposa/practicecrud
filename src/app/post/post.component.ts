996262e771fe83d2648b139aab1d851e0898a071
Jaded <jaduya678@gmail.com>
2023-10-04 11:57:14 +0800

    progress, dae pa ini tapos

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PostUtility } from 'src/post-utility.service';
+import { Post } from 'src/post.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
 
+  constructor(private postUtility: PostUtility, private router: Router, private actRoute: ActivatedRoute){}
+
+  @Input() index: number = 0;
+  @Input() post?: Post;
+
+  ngOnInit(): void {
+    console.log(this.post);
+  }
+  delete(){
+    this.postUtility.deletePindutan(this.index)
+  }
 }
--- src/post-utility.service.ts
+++ src/post-utility.service.ts
@@ -6,6 +6,17 @@ import { Post } from "./post.model";
 })
 export class PostUtility {
 
-    
+    listOfNames: Post[] = [
+
+    ];
+    deletePindutan(index:number){
+        this.listOfNames.splice(index, 1);
+    }
+    addPindutan(post: Post){
+        this.listOfNames.push(post);
+    }
+    updatePindutan(index:number, post: Post){
+        this.listOfNames[index] = post
+    }
 
 }
